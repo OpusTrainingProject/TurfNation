3ac9d66aa62b0d30799bf14f15e07ea7ae3e4304
tushardhamak <tushardhamak657@gmail.com>
2025-11-17 22:12:09 +0530

    Add average rating and count methods to ReviewRepository

--- backend/ReviewService/ReviewService/src/main/java/com/review/Dao/ReviewRepository.java
+++ backend/ReviewService/ReviewService/src/main/java/com/review/Dao/ReviewRepository.java
@@ -3,6 +3,8 @@ package com.review.Dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.review.Entity.Review;
@@ -10,6 +12,11 @@ import com.review.Entity.Review;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-	 List<Review> findByTurfId(Long turfId);
+	    List<Review> findByTurfId(Long turfId);
+	  
+	    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.turfId = :turfId")
+	    Double getAverageRatingByTurfId(@Param("turfId") Long turfId);
+	   
+	    Long countByTurfId(Long turfId);
 	
 }
